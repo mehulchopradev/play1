b2687648a6273b86f30405d6b6545cc9b4988ba3
mehul chopra <mehul.chopra.dev@gmail.com>
2017-07-27 09:56:36 +0530

    call the date api from hello

--- hello.py
+++ hello.py
@@ -5,3 +5,5 @@ print 'Good Afternoon'
 print 'I call the method from the class defined in my-api file'
 
 print 'I call the new api from the project'
+
+print 'Call the date to find the current date'
